35c2b4f0a9f9a2ddaa6a8e8e09f7d08342e3dea9
asgharali97 <asgharalifs48@gmail.com>
2025-05-23 15:33:47 -0700

    Add SignIn component and update Login component layout; include Radix UI dependencies

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,13 +1,14 @@
-import { useNavigate } from "react-router-dom"
-
+import { Outlet } from "react-router-dom"
 function App() {
-   const navigate =  useNavigate()
   return (
+    // color pelette
+    // bg-[#030712] 
+    // mian color #6d28d9
+    // normal text color #808793
     <>
-      <h1 className="text-orange-600 text-3xl bg-zinc-900 h-screen w-full">This is EventSphere</h1>
-      <button onClick={() => navigate('/login')}>
-        Login
-      </button>
+      <div className="w-full bg-[#030712] text-white">
+         <Outlet/>
+      </div>
     </>
   )
 }
--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -3,8 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './components/Login.jsx'
+import SignIn from './components/SignIn.jsx'
 import HeroSection from './components/HeroSection.jsx'
+import Login from './components/Login'
 
 
 const router = createBrowserRouter([
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "/signin",
+        element: <SignIn />,
+      },
       {
         path: "/login",
         element: <Login />,
